df267e39bbce70dacbf83e769ebe8b68096338e7
JonW91 <65132735+JonW91@users.noreply.github.com>
2020-05-10 21:01:29 +0100

    Ciao

--- Terrence/Terrence/Program.cs
+++ Terrence/Terrence/Program.cs
@@ -6,7 +6,7 @@ namespace Terrence
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Ciao Mundo!");
         }
     }
 }
